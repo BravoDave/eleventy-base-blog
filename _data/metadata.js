7825660a3552e2abd75c9b15c19375e2f06ff508
David Varazinskis <dvarazinskis@gmail.com>
2023-06-07 20:02:57 +0100

    2

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,6 +1,6 @@
 module.exports = {
 	title: "Eleventy Base Blog v8",
-	url: "https://example.com/",
+	url: "https://timely-jalebi-a69f8f.netlify.app/",
 	language: "en",
 	description: "I am writing about my experiences as a naval navel-gazer.",
 	author: {
