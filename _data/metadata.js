883233787ecf0548f8d13167743a6ecd081cbb7d
David Varazinskis <dvarazinskis@gmail.com>
2023-06-14 20:44:44 +0100

    another message

--- _data/metadata.js
+++ _data/metadata.js
@@ -2,7 +2,7 @@ module.exports = {
 	title: "Eleventy Base Blog v8",
 	url: "https://timely-jalebi-a69f8f.netlify.app/",
 	language: "en",
-	description: "Im learning to be a coder. Edit 2",
+	description: "Im learning to be a coder. Edit 2 another edit to test",
 	author: {
 		name: "David Varazinskis",
 		email: "dvarazinskis@gmail.com",
